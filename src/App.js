d4867d9f3972af2d6746c67ec1d5e45c081cd35e
Edwin <miguepinchao1992@gmail.com>
2022-12-05 07:38:48 -0600

    Funcionalidad de Biblioteca y stylesJS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Create from './pages/Create';
+import Index from './pages/Index';
+import View from './pages/View';
+import Store from './store/store';
 
 function App() {
   return (
-    <div className="App">
 
-    </div>
+      <Store>
+      <Router>
+      <Routes>
+        <Route path='/' element={<Index/>}/>
+        <Route path='create' element={<Create/>}/>
+        <Route path='view/:bookId' element={<View/>}/>
+      </Routes>
+      </Router>
+      </Store>
+    
   );
 }
 
